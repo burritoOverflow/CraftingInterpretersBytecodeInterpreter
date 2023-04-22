245ad0173b668ac2594e30e20724ab7c4eef55f9
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-04-22 15:07:26 -0400

    Formatting fix.

--- src/memory.c
+++ src/memory.c
@@ -3,17 +3,17 @@
 #include "memory.h"
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
-  // deallocation is handled here as well
-  if (newSize == 0) {
-    free(pointer);
-    return NULL;
-  }
+    // deallocation is handled here as well
+    if (newSize == 0) {
+        free(pointer);
+        return NULL;
+    }
 
-  // actual reallocation case
-  void* result = realloc(pointer, newSize);
-  if (result == NULL) {
-    exit(1);
-  }
+    // actual reallocation case
+    void* result = realloc(pointer, newSize);
+    if (result == NULL) {
+        exit(1);
+    }
 
-  return result;
+    return result;
 }
--- src/value.c
+++ src/value.c
@@ -5,30 +5,29 @@
 
 // initialize to a `zero` state
 void initValueArray(ValueArray* array) {
-  array->values = NULL;
-  array->capacity = 0;
-  array->count = 0;
+    array->values = NULL;
+    array->capacity = 0;
+    array->count = 0;
 }
 
 void writeValueArray(ValueArray* array, Value value) {
-  if (array->capacity < array->count + 1) {
-    // grow in the same fashion as the Chunk
-    const int oldCapacity = array->capacity;
-    array->capacity = GROW_CAPACITY(oldCapacity);
-    array->values =
-        GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
-  }
+    if (array->capacity < array->count + 1) {
+        // grow in the same fashion as the Chunk
+        const int oldCapacity = array->capacity;
+        array->capacity = GROW_CAPACITY(oldCapacity);
+        array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
+    }
 
-  array->values[array->count] = value;
-  array->count++;
+    array->values[array->count] = value;
+    array->count++;
 }
 
 void freeValueArray(ValueArray* array) {
-  // set the value array to a `zero state`
-  FREE_ARRAY(Value, array->values, array->capacity);
-  initValueArray(array);
+    // set the value array to a `zero state`
+    FREE_ARRAY(Value, array->values, array->capacity);
+    initValueArray(array);
 }
 
 void printValue(Value value) {
-  printf("%g", value);
+    printf("%g", value);
 }
